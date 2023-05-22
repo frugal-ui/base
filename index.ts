042fd32887e6b874a91744c786a9a371355d9080
viridian035 <viridian035@gmail.com>
2023-05-22 16:56:57 +0000

    prepare state

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
 /* REACTIVITY */
 // GENERAL
+export type ValueObject<T> = T | State<T>;
+
+function unwrapValue<T>(valueObject: ValueObject<T>) {
+    if (valueObject instanceof State) return valueObject.value;
+    else return valueObject;
+}
 
 // STATE
+export class State<T> {
+    private _value: T;
+
+    constructor(value: T) {
+        this._value = value;
+    }
+
+    /* basic */
+    get value() {
+        return this._value;
+    }
+}
 
 /* COMPONENTS */
 // GENERAL
