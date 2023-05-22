2d7cd74b185b1f2925d7081a7ca5eb4505d29e56
viridian035 <viridian035@gmail.com>
2023-05-22 17:09:14 +0000

    finish states

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+export function UUID() {
+    return uuidv4();
+}
+
 /* REACTIVITY */
 // GENERAL
 export type ValueObject<T> = T | State<T>;
@@ -8,8 +13,18 @@ function unwrapValue<T>(valueObject: ValueObject<T>) {
 }
 
 // STATE
+export interface StateResponseDetails {
+    safeToPropagate: boolean;
+}
+export type StateResponder = <T>(newValue: T, details: StateResponseDetails) => void;
+export interface StateBinder<T> {
+    uuid: typeof UUID;
+    responder: StateResponder;
+}
+
 export class State<T> {
     private _value: T;
+    private binders = new Map<StateBinder<T>['uuid'], StateBinder<T>['responder']>();
 
     constructor(value: T) {
         this._value = value;
@@ -18,6 +33,26 @@ export class State<T> {
     /* basic */
     get value() {
         return this._value;
+    }
+
+    set value(newValue: T) {
+        this._value = newValue;
+        this.trigger({
+            safeToPropagate: true,
+        });
+    }
+
+    /* reactivity */
+    trigger(options: StateResponseDetails) {
+        this.binders.forEach(responder => {
+            responder(this.value, options);
+        })
+    }
+    addBinder(binder: StateBinder<T>) {
+        this.binders.set(binder.uuid, binder.responder);
+    }
+    removeBinder(binder: StateBinder<T>) {
+        this.binders.delete(binder.uuid);
     }
 }
 
