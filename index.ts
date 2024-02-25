0387e1f5e365db72085182c2c3acc0bc43b5afdb
marlonerler <marlon@erler.cc>
2024-02-25 18:34:30 +0100

    add documentation

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 /*
 	REACTIVITY
 */
+// STATE
+/**
+ * A State's purpose is to hold a value of type T and allow getting and setting this value.
+ * Every time the value is changed, the the State triggers all subscribing functions (cf. StateSubscription).
+ */
 class State<T> {
     private _value: T;
     private _bindings = new Set<StateSubscription<T>>();
@@ -9,17 +14,29 @@ class State<T> {
         this._value = initialValue;
     }
 
+	/**
+	 * Gets the current value of the state
+	 */
     get value(): T {
         return this._value;
     }
+	/**
+	 * Sets a new value for the state and calls all subscribing functions
+	 */
     set value(newValue: T) {
         this._value = newValue;
         this._bindings.forEach((fn) => fn(this._value));
     }
 
+	/**
+	 * Adds a subscribing function (StateSubscription)
+	 */
     subscribe(fn: StateSubscription<T>) {
         this._bindings.add(fn);
     }
+	/**
+	 * Removes a subscribing function (StateSubscription)
+	 */
     unsubscribe(fn: StateSubscription<T>) {
         this._bindings.delete(fn);
     }
